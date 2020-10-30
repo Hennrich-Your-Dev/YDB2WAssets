7bf7666214678e184423705373e2bf763c258c4e
Douglas Hennrich <douglashennrich@yourdev.com.br>
2020-10-30 18:54:09 -0300

    First commit

---
+++ YDB2WAssets/UIImageExtension.swift
@@ -0,0 +1,21 @@
+//
+//  UIImageExtension.swift
+//  YDB2WAssets
+//
+//  Created by Douglas Hennrich on 30/10/20.
+//
+
+import UIKit
+
+private let bundle = Bundle.init(identifier: "br.com.yourdev.YDB2WAssets")
+
+public extension UIImage {
+
+  struct Icons {
+
+    static var rightArrow: UIImage? {
+      return UIImage(named: "rightArrow", in: bundle, compatibleWith: nil)
+    }
+    
+  }
+}
