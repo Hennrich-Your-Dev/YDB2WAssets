17ca6b74e184644c827fd96de927fe722da19f53
Douglas Hennrich <douglashennrich@yourdev.com.br>
2020-11-09 14:15:13 -0300

    updating

--- YDB2WAssets/UIImageExtension.swift
+++ YDB2WAssets/UIImageExtension.swift
@@ -11,4 +11,8 @@ public class Icons {
   private static let podsBundle = Bundle(for: Icons.self)
 
   public static let rightArrow = UIImage(named: "rightArrow", in: podsBundle, compatibleWith: nil)
+
+  public static let starYellow = UIImage(named: "starYellow", in: podsBundle, compatibleWith: nil)
+
+  public static let starGrey = UIImage(named: "starGrey", in: podsBundle, compatibleWith: nil)
 }
